c772fafab5ffd4018f1b227457cd88de65613c88
Maria Dudina <mariadudina@MacBook-Air-Maria.local>
2025-11-03 10:33:28 +0100

    Add vite.config.js, handlebars

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import handlebars from 'vite-plugin-handlebars';
+
+export default defineConfig({
+  root: resolve(__dirname, 'src'),
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        login: resolve(__dirname, 'src/pages/login.html')
+      }
+    }
+  },
+  plugins: [handlebars({
+    context: {
+        title: 'Praktikum Messenger',
+      },
+  })],
+}) 
