f8276e22b55354f4a3c274dfc8b522ef0126e6bf
Maria Dudina <mariadudina@MacBook-Air-Maria.local>
2025-11-24 21:40:25 +0100

    Remove input partial

--- src/components/input/input.ts
+++ src/components/input/input.ts
@@ -1,5 +1,6 @@
 import Block from '../../services/block';
 import inputTemplate from './input.template';
+import './input.scss';
 
 export default class Input extends Block {
 	render(): DocumentFragment {
